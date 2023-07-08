f816f6e11ddfc1b6e27cbc2cf50e8c5473352356
gvarys <johnnyzaev@gmail.com>
2023-07-08 20:19:49 +0600

    now ball fly properly

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,8 +4,11 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
 	[SerializeField] private Rigidbody2D currentBallRigidbody;
+	[SerializeField] private SpringJoint2D currentBallSpringJoint;
+	[SerializeField	] private float delayDuration = 0.5f;
 	
 	private Camera _mainCamera;
+	private bool _isDragging;
 
 	private void Start()
 	{
@@ -14,17 +17,39 @@ public class BallHandler : MonoBehaviour
 
 	private void Update()
 	{
+		if (currentBallRigidbody == null)
+			return;
 		if (!Touchscreen.current.primaryTouch.press.isPressed)
 		{
-			currentBallRigidbody.isKinematic = false;
+			if (_isDragging)
+				LaunchBall();
+
+			_isDragging = false;
 			return;
 		}
-		
+
+		_isDragging = true;
 		currentBallRigidbody.isKinematic = true;
 		
 		Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 		Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
 		
 		currentBallRigidbody.position = worldPosition;
+	}
+
+	private void LaunchBall()
+	{
+		currentBallRigidbody.isKinematic = false;
+		currentBallRigidbody = null;
+		
+		Invoke	(nameof(DetachBall), delayDuration);
+		
+
+	}
+
+	private void DetachBall()
+	{
+		currentBallSpringJoint.enabled = false;
+		currentBallSpringJoint = null;
 	}
 }
