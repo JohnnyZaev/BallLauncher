fa35c39bae8aa982924fc24cb589fa58ac0f98d6
gvarys <johnnyzaev@gmail.com>
2023-07-08 19:57:44 +0600

    now touch look to world position

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,12 +4,21 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+	private Camera _mainCamera;
+
+	private void Start()
+	{
+		_mainCamera = Camera.main;
+	}
+
 	private void Update()
 	{
 		if (!Touchscreen.current.primaryTouch.press.isPressed)
 			return;
 		Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+		Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
 		
-		Debug.Log(touchPosition);
+		Debug.Log(worldPosition);
 	}
 }
